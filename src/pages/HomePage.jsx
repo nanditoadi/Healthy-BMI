3531f2c47a0dbb3c98b1d0efa162c0ecb35176ee
Irfani <369indonesia@gmail.com>
2025-04-25 22:48:16 +0700

    fungsionalitas
    menambahkan fungsional hitung bmi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+
 const HomePage = () => {
   return (
     <div>HomePage
-        <h2>infoo</h2>
-        <h3>krass</h3>
+        <h2>BMI Calculator</h2>
+        <label for="weight">bobot (kg):</label>
+        <input type="number" id="weight" placeholder="bobot (kg)"></input>
+        <br />
+        <label for="height">tinggi (cm):</label>
+        <input type="number" id="height" placeholder="tinggi (cm)"></input>
     </div>
     
   )
