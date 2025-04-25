5ed411e59d63f4d87ff4de0c6464caaea9cb058b
Irfani <369indonesia@gmail.com>
2025-04-25 18:21:42 +0700

    test 2
    test 2

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+const HomePage = () => {
+    return (
+      <div>
+        <h2>ntnt</h2>
+        {/* <label htmlFor="weight"></label>
+        <h2>Healty-BMI</h2>
+        <label For="weight"> bobot (kg)</label>
+        <input type="number" id="weight" placeholder="masukkan bobot kg"></input>
+        <br />
+        <label htmlfor="height">tinggi (cm):</label>
+        <input type="number" id="height" placeholder="masukkan tinggi cm"></input>
+        <br />
+        <button onclick="calculateBMI()">hitung BMI</button>
+        <div id="result"></div> */}
+        <h1>test</h1>
+      </div>
+    )
+  }
+  
+  export default HomePage
