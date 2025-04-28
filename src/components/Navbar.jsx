b488e3e7b7e0e89637e153a66bee4298119ee2eb
azzm04 <168269811+azzm04@users.noreply.github.com>
2025-04-29 02:47:25 +0700

    nambah beranda dan tentang BMI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,66 @@
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+  const [show, setshow] = useState(false);
+  const [scroll, setScroll] = useState(false);
+
+  const handleClick = () => {
+    setshow(!show);
+    //console.log(show);
+  };
+
+
+  let menuActive = show ? "left-0" : "-left-full";
+
+
+  useEffect(() =>(
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 5) {
+        setScroll(true);
+        setshow(false);
+      } else {
+        setScroll(false);
+      }
+    })
+  ))
+
+  let scrollActive = scroll ? "py-6 bg-white shadow" : "py-4";
+  
   return (
-    <div className="navbar fixed w-full transition-all py-4">
+    <div className={`navbar fixed w-full transition-all ${scrollActive}`}>
       <div className="container mx-auto px-4">
         <div className="navbar-box flex items-center justify-between">
           <div className="logo flex items-center gap-3">
           <img src="/logonavbar.png" alt="Healthy BMI Logo" className="w-10 h-10 object-contain align-middle" />
-            <h1 className="text-2xl font-bold">Healthy BMI</h1>
+            <h1 className="sm:text-2xl text-xl font-bold">Healthy BMI</h1>
           </div>
-          <ul className="flex flex-col gap-8 fixed left-0 top-1/2 -translate-y-1/2 px-8 py-6 rounded shadow-lg shadow-slate-300 bg-[#C7DEFF] font-bold text-black transition-all md:flex-row md:gap-12 md:static md:translate-y-0 md:top-0 md:left-0 md:w-auto md:h-auto md:p-0 md:m-0 md:shadow-none md:bg-transparent">
-            <li>
+          <ul 
+          className={`flex lg:gap-12 md:static flex-col gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 px-8 py-6 rounded shadow-lg shadow-slate-300 bg-[#C7DEFF] font-bold text-black transition-all md:flex-row md:gap-12 md:static md:translate-y-0 md:top-0 md:left-0 md:w-auto md:h-auto md:p-0 md:m-0 md:shadow-none md:bg-transparent`}>
+            <li className="flex items-center gap-3">
+              <i className="ri-home-5-line text-3xl md:hidden block"></i>
               <a href="#"className="font-medium opacity-75">
                 Beranda</a> 
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+              <i className="ri-stack-line text-3xl md:hidden block"></i>
               <a href="#"className="font-medium opacity-75">
                  Tentang BMI</a> 
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+              <i className="ri-service-line text-3xl md:hidden block"></i>
               <a href="#"className="font-medium opacity-75">
                 Cek BMI</a> 
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+              <i className="ri-send-plane-2-line text-3xl md:hidden block"></i>
               <a href="#"className="font-medium opacity-75">
                 Kritik & Saran</a> 
             </li>
 
           </ul>
-          <div className="Kontak">
+          <div className="Kontak flex items-center gap-2">
             <a href="#" className="px-5 py-2 rounded-full text-black font-bold bg-[#C7DEFF] hover:bg-[#A6CBFF] transition-all" >Hubungi Kami</a>
+            <i className="ri-menu-line text-3xl md:hidden block" onClick={handleClick}></i>
           </div>
         </div>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,51 @@
+import HeroImage from "../assets/coba.svg";
+import AboutImage from "../assets/komponen orang.svg";
+
+
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="homepage pb-10">
+      <div className="container mx-auto px-4">
+        <div className="hero grid md:grid-cols-2 grid-cols-1 items-center gap-20 pt-32">
+          <div className="box">
+            <h1 className="lg:text-5xl/tight text-3xl font-medium mb-7">
+              Cek <span className="font-bold text-000000 ">BMI</span> Sekarang
+            </h1>
+            <p className="text-base/8 mb-7 ">Temukan <span className="font-semibold text-000000">Rekomendasi Khusus untuk Anda</span></p>
+            <a href="#" className="inline-flex items-center gap-2 bg-gradient-to-r from-[#F9EC7D] to-[#F5B100] hover:opacity-90 shadow-[0_4px_20px_rgba(245,177,0,0.5)] tranisiton-all px-4 py-2 text-black font-semibold  shadow rounded-full w-auto ">
+               Cek BMI Sekarang <i className="w-6 h-6 bg-black rounded-full flex items-center justify-center ri-arrow-right-wide-line text-white ms-1"></i>
+            </a>
+          </div>
+          <div className="box">
+            <img src={HeroImage} alt="HeroImage" className="md:w-[500px] w-[400px] mx-auto md:m-0"/>
+          </div>
+        </div>
 
-export default HomePage
+        <div className="about grid md:grid-cols-2 grid-cols-1 items-center gap-16 pt-20 pt-32">
+          <div className="box order-2 md:order-1 ">
+            <h1 className="text-3xl md:text-4xl font-medium text-center md:text-left mb-6 -translate-y-6 md:-translate-y-10">
+              Apa itu <span className="font-bold">BMI</span>?
+            </h1>
+
+            <div className="inline-block bg-[#C7DEFF] text-black font-bold px-6 py-3 rounded-3xl mb-7 text-lg">
+              BMI (Body Mass Index)
+            </div>
+
+            <p className="text-base leading-relaxed text-justify mb-4">
+            Body Mass Index (BMI) atau Indeks Massa Tubuh (IMT) adalah indikator pengukuran untuk memperkirakan berat badan ideal seseorang berdasarkan tinggi dan berat badannya, untuk mengetahui apakah berat badan seseorang termasuk normal, kurus, atau obesitas.
+            </p>
+            <p className="text-base leading-relaxed text-justify">
+            Indeks BMI untuk mengukur status gizi dewasa yaitu usia diatas 18 tahun, basis perhitungannya BB(kg)/TB²(m). Sedangkan untuk mengukur status gizi anak dan remaja usia 0-18 tahun menggunakan indeks BMI/A (BMI for Age). BMI/A basis perhitungannya yaitu dengan Z-Score.
+            </p>
+          </div>
+          <div className="box md:order-1 order-2">
+            <img src={AboutImage} alt="About Image" className="lg:w-[250px] w-[150px] md:w-[220px] mx-auto"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
