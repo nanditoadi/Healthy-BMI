946fc210088e45bd384245390bc29d1f18c867a9
nanditoadi <nanditoadi42@gmail.com>
2025-06-07 22:11:46 +0700

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 import HomePage from "./pages/HomePage"
 import React, { useState } from 'react';
 
